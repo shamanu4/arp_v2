33844a700d16f1230d15dade4110b06eeb0764ff
Your Name <shamanu4@gmail.com>
2012-06-19 11:20:22 +0300

    postgis template creation script fix 2

--- lib/postgis/create_template_postgis-1.5.sh
+++ lib/postgis/create_template_postgis-1.5.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 #POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-1.5
 POSTGIS_SQL_PATH=/usr/pgsql-9.0/share/contrib/postgis-1.5
+PSQL_ADV_LIB_PATH=/usr/pgsql-9.0/lib/
+PSQL_LIB_PATH=`pg_config --libdir`
+
+cd -- "PSQL_ADV_LIB_PATH"
+find -type f -exec ln --symbolic -- "$PSQL_ADV_LIB_PATH"/{} "$PSQL_LIB_PATH"/{} \;
+
 createdb -E UTF8 template_postgis # Create the template spatial database.
 createlang -d template_postgis plpgsql # Adding PLPGSQL language support.
 psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
